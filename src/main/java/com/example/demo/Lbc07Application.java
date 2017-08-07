3a806632f8fba16e1118536e4e98b014685b4804
Sayed Naved <sayednaved@gmail.com>
2017-08-07 19:53:56 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Lbc07Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lbc07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lbc07Application.class, args);
+	}
+}
